721cfd017feaa348a5784fd26d5ce084d1af99a6
Debi <debiskea@live.co.uk>
2018-06-10 10:32:31 +0100

    adds rentals controller

--- app.rb
+++ app.rb
@@ -1,11 +1,12 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative('controllers/customer_controller')
+require_relative('controllers/stock_controller')
 
 configure do
   enable :method_override
 end
 
 get '/' do #home page view
-  erb( :"customer_views/home" )
+  erb( :"/home" )
 end
--- controllers/stock_controller.rb
+++ controllers/stock_controller.rb
@@ -3,3 +3,8 @@ require('sinatra/contrib/all')
 require('pry')
 require_relative('../models/stock')
 also_reload('../models/*')
+
+get '/stock' do #view all stock
+  @stock = Stock.all()
+  erb( :"/stock_views/stock" )
+end
