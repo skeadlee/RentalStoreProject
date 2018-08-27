87fbd604e671bad781bc3d9559c04a6c4071b5d5
Debi <debiskea@live.co.uk>
2018-08-27 16:20:13 +0100

    adds line to app.rb

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require( 'sinatra' )
-require( 'sinatra/contrib/all' )
+require('sinatra/contrib/all') if development?
+# require( 'sinatra/contrib/all' )
 require_relative('controllers/customer_controller')
 require_relative('controllers/stock_controller')
 
