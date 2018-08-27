af49d61ac598b1476a76546d918a91228d14c8c2
Debi <debiskea@live.co.uk>
2018-08-27 16:40:55 +0100

    removes the if development lines from app.rb

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 require( 'sinatra' )
 require('sinatra/contrib/all') if development?
 # require( 'sinatra/contrib/all' )
-require_relative('controllers/customer_controller') if development?
-require_relative('controllers/stock_controller') if development?
+require_relative('controllers/customer_controller')
+require_relative('controllers/stock_controller') 
 
 # configure do
 #   enable :method_override
